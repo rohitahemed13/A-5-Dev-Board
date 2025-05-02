dd6dd9460e864bc87d3fe8673943e7c5eaa59b59
rohitahemed <rohitzone884@gmail.com>
2025-05-03 02:19:03 +0600

    first commit

---
+++ scripts/back.js
@@ -0,0 +1,4 @@
+document.getElementById('newPage').addEventListener('click',function(event){
+    event.preventDefault();
+     window.location.href = "index.html"
+});
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document.getElementById('newfile').addEventListener('click',function(event){
+    event.preventDefault();
+    window.location.href = "main.html"
+   
+});
+
+document.getElementById('newPage').addEventListener('click',function(event){
+    event.preventDefault();
+     window.location.href = "index.html"
+});
+
