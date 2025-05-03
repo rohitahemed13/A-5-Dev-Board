ce7bdbb33c1f27e7aea68629229bc02533db4361
rohitahemed <rohitzone884@gmail.com>
2025-05-03 20:01:31 +0600

    added a new topic

---
+++ scripts/card1.js
@@ -0,0 +1,50 @@
+document.getElementById('btn-complite').addEventListener('click',function(event){
+    event.preventDefault();
+    alert('Board updating succesfully');
+ let taksinput = document.getElementById('taks-Input');
+ let currentValue = taksinput.textContent.trim();
+ let numberValue = Number(currentValue);
+ if(!isNaN(numberValue)){
+    numberValue += 1;
+    taksinput.textContent =numberValue;
+ }else{
+    alert('Navbar value is not a valid number !');
+ }
+   
+  });
+
+  document.getElementById('btn-complite').addEventListener('click',function(event){
+    event.preventDefault();
+    let takslist = document.getElementById('task-list');
+    let currentValue = takslist.textContent.trim();
+    let numberValue = Number(currentValue);
+    if(!isNaN(numberValue)){
+        numberValue -=1;
+        takslist.textContent = numberValue;
+    }else{
+        alert('Invalid number');
+    }
+  });
+
+  document.getElementById('btn-complite').addEventListener('click',function(event){
+    event.preventDefault();
+    
+document.getElementById('btn-complite').style.gap= '10px'
+   let time = new
+        Date().toLocaleTimeString();
+        let result = 'You have complete the taks  ' +  'Fix Mobile Button Issue'  + 'at'+ time;
+     
+
+        document.getElementById('transection-container').innerHTML+= '<p>' +  result  + '<p>'; 
+        
+        document.getElementById('transection-container').style.backgroundColor = '#F4F7FF';
+        document.getElementById('transection-container').style.padding = '5px';
+        document.getElementById('transection-container').style.fontSize = '16px';
+        document.getElementById('transection-container').style.width = '80%';
+        document.getElementById('transection-container').style.height = 'auto';
+        document.getElementById('transection-container').style.gap = '10px';
+        document.getElementById('transection-container').style.borderRadius = '10px';
+        
+        
+        
+});
---
+++ scripts/card4.js
@@ -0,0 +1,46 @@
+document.getElementById('btn-emoji').addEventListener('click',function(event){
+    event.preventDefault();
+    alert('Board updating succesfully');
+ let taksinput = document.getElementById('taks-Input');
+ let currentValue = taksinput.textContent.trim();
+ let numberValue = Number(currentValue);
+ if(!isNaN(numberValue)){
+    numberValue += 1;
+    taksinput.textContent =numberValue;
+ }else{
+    alert('Navbar value is not a valid number !');
+ }
+   
+  });
+
+document.getElementById('btn-emoji').addEventListener('click',function(event){
+    event.preventDefault();
+    let takslist = document.getElementById('task-list');
+    let currentValue = takslist.textContent.trim();
+    let numberValue = Number(currentValue);
+    if(!isNaN(numberValue)){
+        numberValue -=1;
+        takslist.textContent = numberValue;
+    }else{
+        alert('Invalid number');
+    }
+  });
+
+  document.getElementById('btn-emoji').addEventListener('click',function(event){
+    event.preventDefault();
+    
+
+   let time = new
+        Date().toLocaleTimeString();
+        let result = 'You have complete the taks  ' +  'Add new emoji 🤲 '  + 'at'+ time;
+
+        document.getElementById('transection-container').innerHTML+= '<p>' + result + '<p>'; 
+        document.getElementById('transection-container').style.backgroundColor = '#F4F7FF';
+        document.getElementById('transection-container').style.padding = '10px';
+        document.getElementById('transection-container').style.fontSize = '16px';
+        document.getElementById('transection-container').style.width = '80%';
+        document.getElementById('transection-container').style.height = 'auto';
+        document.getElementById('transection-container').style.gap = '10px';
+        document.getElementById('transection-container').style.borderRadius = '10px';
+        
+});
